3a8a4044cddcf543e5c3ebde4c80874eea8312d7
llleeeaaannn <piercefrank10@gmail.com>
2023-04-01 16:19:51 +0100

    Refacoted middleware into seperate concerns

--- routes/holdings.js
+++ routes/holdings.js
@@ -3,7 +3,7 @@ import XLSX from 'xlsx';
 
 const router = express.Router();
 
-const testMiddleware = async function(req, res, next) {
+const getHoldings = async function(req, res, next) {
   try {
 
     const workbook = XLSX.readFile('spyholdings.xlsx');
@@ -15,6 +15,19 @@ const testMiddleware = async function(req, res, next) {
     const data = XLSX.utils.sheet_to_json(sheet);
 
     const tickerData = data.slice(3, data.length - 6);
+
+    req.data = tickerData;
+
+    next();
+  } catch(error) {
+    next(error);
+  }
+}
+
+const formatHoldings = async function(req, res, next) {
+  try {
+
+    const tickerData = req.tickerData;
 
     const formattedData = tickerData.map(item => {
       const ticker = item['SPDR® S&P 500® ETF Trust'];
@@ -31,7 +44,7 @@ const testMiddleware = async function(req, res, next) {
       };
     });
 
-    req.data = formattedData;
+    req.formattedData = formattedData;
 
     next();
   } catch(error) {
@@ -41,8 +54,8 @@ const testMiddleware = async function(req, res, next) {
 
 router
   .route('/')
-  .get([testMiddleware], function(req, res) {
-    res.send(req.data)
+  .get([getHoldings, formatHoldings], function(req, res) {
+    res.send(req.formattedData)
   })
 
 export default router;
