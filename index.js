25412e39379ed07a4299c45ca79cb7997f30579f
llleeeaaannn <piercefrank10@gmail.com>
2023-04-01 14:31:43 +0100

    Added error handling function

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const errorHandler = function(error, req, res, next) {
+  console.log(`Error: ${error.message}`);
+  res.status(404);
+  res.send('Invalid Path');
+}
+
 app.use('/holdings', holdings);
 
 app.listen(PORT, () => console.log(`Server running on port ${Port}`));
