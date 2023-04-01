948641b98899bfe95c57e79434feb257d645657b
llleeeaaannn <piercefrank10@gmail.com>
2023-04-01 14:29:57 +0100

    Added logic for API to listen on port and route

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ import holdings from './routes.holdings.js';
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/holdings', holdings);
+
+app.listen(PORT, () => console.log(`Server running on port ${Port}`));
