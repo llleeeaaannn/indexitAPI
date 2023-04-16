ed42568cd8b4f9eafd6a505e781d1effd78bf7da
llleeeaaannn <piercefrank10@gmail.com>
2023-04-16 14:58:41 +0100

    Added function to seperate getting holdings from middleware

---
+++ getHoldingsData.js
@@ -0,0 +1,40 @@
+import XLSX from 'xlsx';
+import express from 'express';
+
+const getHoldingsData = () => {
+
+  // Read holdings XLSX file
+  const workbook = XLSX.readFile('spyholdings.xlsx');
+
+  // Get first name from list of sheet names
+  const sheetName = workbook.SheetNames[0];
+
+  // Get first sheet
+  const sheet = workbook.Sheets[sheetName];
+
+  // Convert XLSX to JSON
+  const data = XLSX.utils.sheet_to_json(sheet);
+
+  // Remove the first three and last six objects
+  const tickerData = data.slice(3, data.length - 6);
+
+  // Function that takes tickerData array, reformats the objects and returns them in an object
+  const formattedData = tickerData.reduce((acc, item) => {
+    const price = Math.floor(Math.random() * (250 - 50 + 1) + 50);
+    const ticker = item['SPDR® S&P 500® ETF Trust'];
+    acc[ticker] = {
+      ticker: ticker,
+      name: item['Fund Name:'],
+      weight: item['__EMPTY_2'],
+      sedol: item['__EMPTY_1'],
+      identifier: item['__EMPTY'],
+      shares: item['__EMPTY_4'],
+      sector: item['__EMPTY_3'],
+      price: price,
+      originalprice: price
+    };
+    return acc;
+  }, {});
+}
+
+export default getHoldingsData
