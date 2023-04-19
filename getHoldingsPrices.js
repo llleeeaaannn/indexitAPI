795f59df6aa01aa43e35a6e9429084fef89cbb5b
Frank Pierce <piercefrank10@gmail.com>
2023-04-19 15:22:36 +0000

    Wrote logic to batch API calls

--- getHoldingsPrices.js
+++ getHoldingsPrices.js
@@ -2,7 +2,12 @@ import fs from 'fs/promises';
 
 const getHoldingsPrices = async () => {
 
-  let holdings = 'Test';
+  const BATCHSIZE = 50;
+  const CALLDELAY = 90 * 1000;
+
+  let stocks;
+
+  const pricedStocks = {};
 
   try {
     // Import Holdings data
@@ -13,10 +18,48 @@ const getHoldingsPrices = async () => {
     console.error('Error reading file:', err);
     return;
   }
+  
+  // Function to make the API call for a single stock
+  async function callApiForStock(stock) {
+    const response = await fetch('https://api.example.com/stocks', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(stock),
+    });
+  
+    const data = await response.json();
+    console.log(`API call result for ${stock.ticker}:`, data);
 
-  console.log(typeof holdings)
-  console.log(holdings)
-
+    pricedStocks[stock.ticker] = data;
+  }
+  
+  // Function to call a batch of stocks
+  async function processBatch(batch) {
+    for (const ticker of batch) {
+      await callApiForStock(stocks[ticker]);
+    }
+  }
+  
+  // Function to iterate over the stocks object and make API calls
+  async function makeApiCalls() {
+    const stockKeys = Object.keys(stocks);
+  
+    for (let i = 0; i < stockKeys.length; i += BATCHSIZE) {
+      const batch = stockKeys.slice(i, i + BATCHSIZE);
+  
+      // Process the current batch
+      await processBatch(batch);
+  
+      // If there are more stocks to process, wait for the delay between calls
+      if (i + BATCHSIZE < stockKeys.length) {
+        await new Promise((resolve) => setTimeout(resolve, CALLDELAY));
+      }
+    }
+  }
+  
+  // Start making the API calls
+  makeApiCalls();
+  
 
   // Seperate data into segments
 
