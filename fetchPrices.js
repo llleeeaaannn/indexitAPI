1bcb4717ecd53c07800d595038a83888704d89a9
llleeeaaannn <piercefrank10@gmail.com>
2023-04-22 23:56:23 +0100

    Commented code

--- fetchPrices.js
+++ fetchPrices.js
@@ -2,22 +2,24 @@ import fs from 'fs/promises';
 
 const getPrices = async () => {
 
+  // Batch size defines size of API call batch and delay between batches
   const BATCHSIZE = 50;
   const CALLDELAY = 90 * 1000;
 
   let stocks;
+
+  // Object of stocks with price data included
   const pricedStocks = {};
 
   try {
     // Import Holdings data
     const data = await fs.readFile('output.json', 'utf8');
     stocks = JSON.parse(data);
-    console.log('JSON got');
   } catch (err) {
     console.error('Error reading file:', err);
     return;
   }
-  
+
   // Function to make the API call for a single stock
   async function callApiForStock(stock) {
     const response = await fetch(`https://api.tradier.com/v1/markets/quotes?symbols=${stock.ticker}`, {
@@ -29,8 +31,8 @@ const getPrices = async () => {
     if (!response.ok) {
       console.log('Response failed')
     } else {
+      // Assign data top pricedStocks object
       const data = await response.json();
-      console.log(data);
       const price = data.quotes.quote.last.toFixed(2);
       pricedStocks[stock.ticker] = stock;
       pricedStocks[stock.ticker].price = price;
@@ -38,31 +40,31 @@ const getPrices = async () => {
       pricedStocks[stock.ticker].change = data.quotes.quote.change_percentage;
     }
   }
-  
+
   // Function to call a batch of stocks
   async function processBatch(batch) {
     for (const ticker of batch) {
       await callApiForStock(stocks[ticker]);
     }
   }
-  
+
   // Function to iterate over the stocks object and make API calls
   async function makeApiCalls() {
     const stockKeys = Object.keys(stocks);
-  
+
     for (let i = 0; i < 30; i += BATCHSIZE) {
       const batch = stockKeys.slice(i, i + BATCHSIZE);
-  
+
       // Process the current batch
       await processBatch(batch);
-  
+
       // If there are more stocks to process, wait for the delay between calls
       if (i + BATCHSIZE < 30) {
         await new Promise((resolve) => setTimeout(resolve, CALLDELAY));
       }
     }
   }
-  
+
   await makeApiCalls();
 
   // Write data with new price to file
