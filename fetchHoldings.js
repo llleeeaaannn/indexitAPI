0cce1c8ba73d58e72d04a173892709f5264a9f75
Frank Pierce <piercefrank10@gmail.com>
2023-04-28 12:00:24 +0000

    Formatting and exported function

--- fetchHoldings.js
+++ fetchHoldings.js
@@ -4,20 +4,22 @@ const URL = 'https://www.ssga.com/us/en/intermediary/etfs/library-content/produc
 const PATH = './SPYHoldings.xlsx';
 
 async function downloadHoldingsFile(url, path) {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
-      }
-  
-      const fileStream = fs.createWriteStream(path);
-      response.body.pipe(fileStream);
-  
-      fileStream.on('finish', () => {
-        console.log(`Downloaded and saved file to ${path}`);
-      });
-  
-    } catch (error) {
-      console.error(`Error downloading file: ${error}`);
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
     }
+
+    const fileStream = fs.createWriteStream(path);
+    response.body.pipe(fileStream);
+
+    fileStream.on('finish', () => {
+      console.log(`Downloaded and saved file to ${path}`);
+    });
+
+  } catch (error) {
+    console.error(`Error downloading file: ${error}`);
   }
+}
+
+export default downloadHoldingsFile;
